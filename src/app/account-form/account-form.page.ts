584b289e90057aa7e7044b15e26fcb07eb861906
Nicolas Lopez <juannicolasl1997@gmail.com>
2024-09-22 13:48:08 -0500

    Finish form design

--- src/app/account-form/account-form.page.ts
+++ src/app/account-form/account-form.page.ts
@@ -1,20 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-account-form',
   templateUrl: './account-form.page.html',
   styleUrls: ['./account-form.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
-export class AccountFormPage implements OnInit {
+export class AccountFormPage implements OnInit{
 
-  constructor() { }
+  accountForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.fb.control('', Validators.required)
+
+    // Inicializamos el formulario con validaciones
+    // this.datosCuentaForm = this.fb.group({
+    //   tipoDocumento: ['', Validators.required],
+    //   numeroDocumento: ['', [Validators.required, Validators.minLength(6)]],
+    //   fechaExpedicion: ['', Validators.required],
+    //   fechaNacimiento: ['', Validators.required],
+    //   genero: ['', Validators.required],
+    //   correo: ['', [Validators.required, Validators.email]],
+    //   confirmarCorreo: ['', [Validators.required, Validators.email]],
+    //   pin: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
+    //   confirmarPin: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
+    // }, { validator: this.matchingFields('correo', 'confirmarCorreo') });
+  }
+
+  // Función para validar que el correo y su confirmación sean iguales
+  matchingFields(field1: string, field2: string) {
+    return (formGroup: FormGroup) => {
+      const control1 = formGroup.controls[field1];
+      const control2 = formGroup.controls[field2];
+      if (control1.value !== control2.value) {
+        control2.setErrors({ notMatch: true });
+      } else {
+        control2.setErrors(null);
+      }
+    };
+  }
+
+  // Validamos el formulario antes de enviar
+  onSubmit() {
+    // if (this.datosCuentaForm.valid) {
+    //   // Aquí iría la lógica para enviar los datos al backend
+    //   console.log(this.datosCuentaForm.value);
+    // }
   }
 
 }
